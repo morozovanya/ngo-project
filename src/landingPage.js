8efd7093e7a3e00456643b356d89ef4d560c4295
Anna <Anytamorozov@gmail.com>
2023-01-05 01:25:02 +0100

    blocks builded, improved spacing

--- src/landingPage.js
+++ src/landingPage.js
@@ -32,27 +32,27 @@ export default function landingPage() {
       <li class="nav-item">
         <a class="nav-link" href="#">SUPPORTERS</a>
         </li>
-    <button class="btn btn-outline-warning mx-3" type="button">DONATE</button>
+    <button class="btn btn-warning mx-3 button-header" type="button">DONATE</button>
     </ul>
   </div>
     </nav>
             
             <div className="row m-5">
                 <div className="col text-center">
-                <h1>MILITARY AID FOR <span className="green">SOLDIERS</span>
-                    <br/> IN UKRAINE</h1>
-                <p className="opacity-50">The task of the military headquarters of the Serhiy Prytula Charitable Foundation is to save as many lives as possible of the defenders of Ukraine and help them liberate the occupier</p>
-                    <button className="btn btn-lg btn-warning col-2 m-auto my-5 p-3">DONATE NOW</button>
+                <h1><strong>MILITARY AID FOR <span className="green">SOLDIERS</span>
+                    <br/> IN UKRAINE</strong></h1>
+                <p className="opacity-50 mt-3">The task of the military headquarters of the Serhiy Prytula Charitable<br/>Foundation is to save as many lives as possible of the defenders<br/>of Ukraine and help them liberate the occupier</p>
+                    <button className="btn btn-lg btn-warning col-2 m-auto my-5 p-3 button">DONATE NOW</button>
                     </div>
             </div>
 
             <div className="row text-center about-block">
                 <div className="col-6 about-text">
-                    <h2 className="mb-4">WE BELIEVE IN DOING <br/> INCREDIBLE</h2>
+                    <h2 className="mb-4"><strong>WE BELIEVE IN DOING <br/> INCREDIBLE</strong></h2>
                     <div className="opacity-50 w-75">Ukrainian Serhiy Prytula put aside his media career to help Ukraine in its war effort. Through his foundation, Prytula has generated nearly €83 million to help the Ukrainian army acquire military equipment.
                         <br /> <br/> + OVER 200 VOLUNTEERS <br/>
                         <br />We are volunteers, people who help our army to have the equipment that the state does not have the possibility to buy or does but very slowly</div>
-                    <button className="btn btn-warning mt-5 p-3">DONATE</button>
+                    <button className="btn btn-warning mt-5 button button-about">DONATE</button>
                 </div>
                 <div className="col-6">
                     <img src={About} alt="about" className="about-image w-100"/>
@@ -61,92 +61,92 @@ export default function landingPage() {
 
             
             <div className="row projects-block">
-                <h2 className="text-center">ONGOING <span className="projects-heading">PROJECTS</span></h2>
-                <p className="text-center">Join our fight for freedom, support our projects!</p>
+                <h2 className="text-center"><strong>ONGOING <span className="projects-heading">PROJECTS</span></strong></h2>
+                <p className="text-center project-sub">Join our fight for freedom, support our projects!</p>
 
                 <div className="d-flex justify-content-around mt-5">
-                    <div className="card col-4">
-                        <img className="card-img-top w-75" src={OpticDevices} alt="#"/>
-                        <div className="card-body">
-                            <h5 className="card-title">MILITARY AMUNITION</h5>
+                    <div className="card col-3 p-4">
+                        <img className="card-img-top w-100" src={OpticDevices} alt="#"/>
+                        <div className="card-body p-0">
+                            <h5 className="card-title m-0"><strong>MILITARY AMUNITION</strong></h5>
                             <p className="card-text">Help in the fight against the occupier</p>
                             <div className="d-flex justify-content-between text-center">
-                                <p opacity-50>GOAL
+                                <p> <span className="opacity-50">GOAL</span>
                                     <br />
                                     <strong>
                                         $500,000
                                     </strong>
                                 </p>
-                                <p opacity-50>RAISED
+                                <p><span className="opacity-50">RAISED</span>
                                     <br />
                                     <strong>
                                         $150,000
                                     </strong>
                                 </p>
-                                <p opacity-50>REMAIN
+                                <p><span className="opacity-50">REMAIN</span>
                                     <br />
                                     <strong>
                                         $350,000
                                     </strong>
                                 </p>
                             </div>
-                            <a href="/" className="btn btn-warning">DONATE</a>
+                            <a href="/" className="btn btn-warning button mt-1 button-project">DONATE</a>
                         </div>
                     </div>
-                    <div className="card col-4">
-                        <img className="card-img-top w-75" src={Medical} alt="#" />
-                        <div className="card-body">
-                            <h5 className="card-title">MEDICAL SUPPLIES</h5>
+                    <div className="card col-3 p-4">
+                        <img className="card-img-top w-100" src={Medical} alt="#" />
+                        <div className="card-body p-0">
+                            <h5 className="card-title m-0"><strong>MEDICAL SUPPLIES</strong></h5>
                             <p className="card-text">To save lives of the soldiers</p>
                             <div className="d-flex justify-content-between text-center">
-                                <p opacity-50>GOAL
+                                <p> <span className="opacity-50">GOAL</span>
                                     <br />
                                     <strong>
                                         $700,000
                                     </strong>
                                 </p>
-                                <p opacity-50>RAISED
+                                <p> <span className="opacity-50">RAISED</span>
                                     <br />
                                     <strong>
                                         $550,000
                                     </strong>
                                 </p>
-                                <p opacity-50>REMAIN
+                                <p> <span className="opacity-50">REMAIN</span>
                                     <br />
                                     <strong>
                                         $150,000
                                     </strong>
                                 </p>
                             </div>
-                            <a href="/" className="btn btn-warning">DONATE</a>
+                            <a href="/" className="btn btn-warning button mt-1 button-project">DONATE</a>
                         </div>
                     </div>
-                    <div className="card col-4">
-                        <img className="card-img-top w-75" src={Aerial} alt="#" />
-                        <div className="card-body">
-                            <h5 className="card-title">AERIAL VEHICLES</h5>
+                    <div className="card col-3 p-4">
+                        <img className="card-img-top w-100" src={Aerial} alt="#" />
+                        <div className="card-body p-0">
+                            <h5 className="card-title m-0"><strong>AERIAL VEHICLES</strong></h5>
                             <p className="card-text">Strengthen the Armed Forces</p>
                             <div className="d-flex justify-content-between text-center">
-                                <p opacity-50>GOAL
+                                <p> <span className="opacity-50">GOAL</span>
                                     <br />
                                     <strong>
                                         $1M
                                     </strong>
                                 </p>
-                                <p opacity-50>RAISED
+                                <p> <span className="opacity-50">RAISED</span>
                                     <br />
                                     <strong>
                                         $489,000
                                     </strong>
                                 </p>
-                                <p opacity-50>REMAIN
+                                <p> <span className="opacity-50">REMAIN</span>
                                     <br />
                                     <strong>
                                         $511,000
                                     </strong>
                                 </p>
                             </div>
-                            <a href="/" className="btn btn-warning">DONATE</a>
+                            <a href="/" className="btn btn-warning button mt-1 button-project">DONATE</a>
                         </div>
                     </div>
                 </div>
@@ -154,10 +154,10 @@ export default function landingPage() {
 
             
             <div className="row text-center beneficiary-block">
-                <h2>BE A BEHEFICIARY</h2>
+                <h2><strong>BE A BEHEFICIARY</strong></h2>
                 <p>There are no more or less important projects!
-                    Donate to the fund's general accounts</p>
-                <button className="btn btn-warning col-2 mx-auto">DONATE NOW</button>
+                    <br/>Donate to the fund's general accounts</p>
+                <button className="btn btn-warning col-2 mx-auto button">DONATE NOW</button>
             </div>
 
 
